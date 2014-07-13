367869b95e9adfba5d04d902d6d691fb12dbb3b5
Yana Mikhaylenko <ymihaylenko@gmail.com>
2014-07-13 12:51:50 +0300

    Added exception handler for all classes

--- src/main/java/config/RestWebConfig.java
+++ src/main/java/config/RestWebConfig.java
@@ -1,12 +1,21 @@
 package config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import web.usermanager.AuthenticationInterceptor;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +26,16 @@ import java.util.List;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"web.rest", "web.usermanager", "core.domain"})
-public class RestWebConfig {
+public class RestWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration registration = registry.addInterceptor(new AuthenticationInterceptor());
+        registration.addPathPatterns("/*");
+        //registration.excludePathPatterns("/rest");
+        registration.excludePathPatterns("/login*");
+        registration.excludePathPatterns("/logout");
+    }
 
     @Bean
     public MappingJackson2HttpMessageConverter getJsonConverter() {
@@ -26,5 +44,14 @@ public class RestWebConfig {
         mediaTypes.add(MediaType.APPLICATION_JSON);
         jsonConverter.setSupportedMediaTypes(mediaTypes);
         return jsonConverter;
+    }
+
+    @Bean
+    public static <T> T retrieveResourceFromResponse(HttpResponse response, Class<T> clazz)
+            throws IOException {
+        String jsonFromResponse = EntityUtils.toString(response.getEntity());
+        ObjectMapper mapper = new ObjectMapper().
+                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper.readValue(jsonFromResponse, clazz);
     }
 }
